2769c46fe2375438c6c4ff3b01a8d9b0ac7cf24a
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2019-10-13 12:57:34 +0200

    Some changes

---
+++ js/diaries.js
@@ -0,0 +1,41 @@
+/* open model  */
+const openModel = () => {
+    const model = document.querySelector('.model');
+    model.style.display = 'block';
+};
+const closeModel = () => {
+    const model = document.querySelector('.model');
+    model.style.display = 'none';
+};
+const openAddModel = () => {
+    const model = document.querySelector('.add-model');
+    model.style.display = 'block';
+};
+const closeAddModel = () => {
+    const model = document.querySelector('.add-model');
+    model.style.display = 'none';
+};
+const openDeleteModel = () => {
+    const model = document.querySelector('.delete-model');
+    model.style.display = 'block';
+};
+const closeDeleteModel = () => {
+    const model = document.querySelector('.delete-model');
+    model.style.display = 'none';
+};
+const openEditModel = () => {
+    const model = document.querySelector('.edit-model');
+    model.style.display = 'block';
+};
+const closeEditModel = () => {
+    const model = document.querySelector('.edit-model');
+    model.style.display = 'none';
+};
+const openUserModel = () => {
+    const model = document.querySelector('.user-model');
+    model.style.display = 'block';
+};
+const closeUserModel = () => {
+    const model = document.querySelector('.user-model');
+    model.style.display = 'none';
+};
