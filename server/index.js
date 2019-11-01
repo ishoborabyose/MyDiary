37f8324ff29f2242722e77be24a7f48e9fe3504d
Ishoborabyose <habimanaolivier6@gmail.com>
2019-11-01 15:24:43 +0200

    bug(heroku: solving heroku issues

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 /* Import packages */
 import express from "express";
 import userRoute from "./routes/user.route";
-import  diaryRoutes from "./routes/diary.route";
+import diaryRoutes from "./routes/diary.route";
 
 const app = express();
 
-const PORT = 2000;
+const PORT = process.env.PORT || 2000;
 
 app.use(express.json());
 
 app.use("/api/v1", userRoute, diaryRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
 
 export default app;
